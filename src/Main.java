428c23516aff3f1e0d07987d89ee088147d7ca5e
Lertos <deetasticgaming@gmail.com>
2022-11-17 16:18:29 -0400

    Creating a private method to check valid phone numbers

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,23 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String response = "-";
 
-        try {
-            PhoneNumber pn = new PhoneNumber("1-555-555-5555", "cell");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return;
-        }
+        boolean phoneNumber = isValidPhoneNumber("1-555-555-5555", "cell");
+        boolean phoneNumber2 = isValidPhoneNumber("1-555-555-5555", "cell");
+        System.out.println(phoneNumber);
+        System.out.println(phoneNumber2);
 
         while(!response.equalsIgnoreCase("q")) {
 
         }
+    }
+
+    private static boolean isValidPhoneNumber(String number, String phoneType) {
+        try {
+            PhoneNumber pn = new PhoneNumber(number, phoneType);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
     }
 }
