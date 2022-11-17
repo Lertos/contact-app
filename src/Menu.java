25c388630e8cbb1a743acac55685be70ff53d2f2
Lertos <deetasticgaming@gmail.com>
2022-11-17 18:45:17 -0400

    Adding a Menu class to hold the menu state

---
+++ src/Menu.java
@@ -0,0 +1,47 @@
+enum MenuState {
+    MAIN,
+    ADD_CONTACT,
+    REMOVE_CONTACT,
+    EDIT_CONTACT,
+    OUTPUT_CONTACTS,
+    DELETE_LIST
+}
+
+public class Menu {
+
+    private MenuState currentState;
+
+    public Menu() {
+        this.currentState = MenuState.MAIN;
+    }
+
+    public void switchState(MenuState state) {
+
+    }
+
+    private void stateMainMenu() {
+
+    }
+
+    private void stateAddContact() {
+
+    }
+
+    private void stateRemoveContact() {
+
+    }
+
+    private void stateEditContact() {
+
+    }
+
+    private void stateOutputContacts() {
+
+    }
+
+    private void stateDeleteContacts() {
+
+    }
+
+
+}
