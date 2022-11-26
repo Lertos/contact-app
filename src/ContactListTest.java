cdb7b0c771e1634ab3e8ad56512ed813475bbba6
Lertos <deetasticgaming@gmail.com>
2022-11-26 16:49:28 -0400

    Adding the addContact test cases

--- src/ContactListTest.java
+++ src/ContactListTest.java
@@ -29,6 +29,22 @@ public class ContactListTest {
 
     @Test
     public void addContact() {
+        assertEquals(1, contactList.size());
+
+        //Testing duplicate names not being allowed
+        assertFalse(contactList.addContact("Dee", "1-555-555-5556", "1-666-666-6667"));
+
+        //Testing duplicate cell number not being allowed
+        assertFalse(contactList.addContact("Jack", "1-555-555-5555", "1-666-666-6667"));
+
+        //Testing duplicate cell number not being allowed
+        assertFalse(contactList.addContact("Sally", "1-555-555-5557", "1-666-666-6666"));
+
+        //Testing incorrect phone format
+        assertFalse(contactList.addContact("Russell", "56-565-56", "1-666-666-6667"));
+
+        //Testing correct format
+        assertFalse(contactList.addContact("Bree", "1-444-444-4444", "1-777-777-7777"));
     }
 
     @Test
